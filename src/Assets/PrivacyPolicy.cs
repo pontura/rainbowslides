42df782cceb548e6fa5887f5b57ee2c3584cabe6
pontura <pontura@gmail.com>
2016-07-27 10:18:20 -0300

    splash

---
+++ src/Assets/PrivacyPolicy.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PrivacyPolicy : MonoBehaviour {
+
+    public GameObject panel;
+    public TextAsset[] dialogContentTexts;
+    private string[] dialogContentTextCaches;
+    public Text contentField;
+    
+	void Start () {
+        panel.SetActive(false);
+        dialogContentTextCaches = new string[dialogContentTexts.Length];
+        for (int i = 0; i < dialogContentTexts.Length; i++)
+        {
+            dialogContentTextCaches[i] = dialogContentTexts[i].text;
+        }        
+	}
+
+    public void Open()
+    {
+        panel.SetActive(true);
+        //foreach (string content in dialogContentTextCaches)
+        //    contentField.text += content;
+    }
+    public void Close()
+    {
+        panel.SetActive(false);
+    }
+}
